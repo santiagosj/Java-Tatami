494b2f421d1b621c356a1a81ec98b96090dab630
santiagosj <sanjs965@gmail.com>
2020-11-11 12:14:48 -0300

    inheritence

---
+++ Inheritance/Shape.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Shape{
+
+   Shape(int lados,double h, double w){
+      
+   }
+   public static void main(String[] args) {
+       
+   }
+}
---
+++ Inheritance/Triangle.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+/**
+ * Triangle
+ */
+public class Triangle extends Shape {
+
+    
+
+    Triangle(){
+        super(3, 0, 0);
+    }
+
+    public void calculateArea(double h, double w){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
